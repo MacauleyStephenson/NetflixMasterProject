36c0157cf9362e4bad17ce357490e649dbd405eb
MacauleyStephenson <MacauleyStephenson@mail.com>
2022-02-16 10:21:10 +0000

    fixed font issue

--- discover-videos/pages/_document..js
+++ discover-videos/pages/_document..js
@@ -1,4 +1,4 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
 	render() {
@@ -7,7 +7,7 @@ class MyDocument extends Document {
 				<Head>
 					<link rel="preconnect" href="https://fonts.googleapis.com" />
 					<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-					<link href="https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap" rel="stylesheet" />
+					<link href="https://fonts.googleapis.com/css2?family=Gideon+Roman&family=Roboto+Slab&display=swap" rel="stylesheet" />
 				</Head>
 				<body>
 					<Main />
