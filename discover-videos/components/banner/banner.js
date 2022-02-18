c76c7f6ac546f2f2c4520c5f6c96a90cb51e65f3
MacauleyStephenson <MacauleyStephenson@mail.com>
2022-02-18 11:20:35 +0000

    binded styling for banner and created the css for the module

--- discover-videos/components/banner/banner.js
+++ discover-videos/components/banner/banner.js
@@ -1,3 +1,5 @@
+import styles from './banner.module.css';
+
 const Banner = (props) => {
 	const { title, subTitle, imgUrl } = props;
 
@@ -6,21 +8,29 @@ const Banner = (props) => {
 		console.log('handleOnPlay');
 	}
 
-	return (<div>
-		<h3>{title}</h3>
-		<h3>{subTitle}</h3>
-		<button onClick={handleOnPlay}>Play</button>
-		<div
-			style={{
-				backgroundImage: `url(${imgUrl})`,
-				width: '100%',
-				height: '100%',
-				position: 'absolute',
-				backgroundSize: 'cover',
-				backgroundPosition: '50% 50%',
-			}}
-		></div>
-	</div>
+	return (
+		<div className={styles.container}>
+			<div className={styles.leftWrapper}>
+				<div className={styles.left}>
+					<h3 className={styles.title}>{title}</h3>
+					<h3 className={styles.subTitle}>{subTitle}</h3>
+					<div className={styles.playBtnWrapper}>
+						<button className={styles.playBtnWithIcon} onClick={handleOnPlay}>Play</button>
+					</div>
+				</div>
+			</div>
+			<div
+				className={styles.bannerImg}
+				style={{
+					backgroundImage: `url(${imgUrl})`,
+					width: '100%',
+					height: '100%',
+					position: 'absolute',
+					backgroundSize: 'cover',
+					backgroundPosition: '50% 50%',
+				}}
+			></div>
+		</div>
 	)
 };
 
