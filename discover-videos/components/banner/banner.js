deb5c545c67fe550d663d504fa8171ec42d43469
MacauleyStephenson <MacauleyStephenson@mail.com>
2022-02-18 12:01:33 +0000

    changed fonts, made it more similar to Netflix

--- discover-videos/components/banner/banner.js
+++ discover-videos/components/banner/banner.js
@@ -12,11 +12,18 @@ const Banner = (props) => {
 		<div className={styles.container}>
 			<div className={styles.leftWrapper}>
 				<div className={styles.left}>
+					<div className={styles.nseriesWrapper}>
+						<p className={styles.firstLetter}>N</p>
+						<p className={styles.series}>S E R I E S</p>
+					</div>
+
 					<h3 className={styles.title}>{title}</h3>
 					<h3 className={styles.subTitle}>{subTitle}</h3>
+
 					<div className={styles.playBtnWrapper}>
-						<button className={styles.playBtnWithIcon} onClick={handleOnPlay}>Play</button>
+						<button className={styles.btnWithIcon} onClick={handleOnPlay}><span className={styles.playText}>Play</span></button>
 					</div>
+
 				</div>
 			</div>
 			<div
