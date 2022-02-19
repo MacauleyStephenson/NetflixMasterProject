e190ed086f1fde0f3406b9eb7d17d31cb614974b
MacauleyStephenson <MacauleyStephenson@mail.com>
2022-02-19 12:06:40 +0000

    connecting navbar styling

--- discover-videos/components/nav/navbar.js
+++ discover-videos/components/nav/navbar.js
@@ -1,23 +1,29 @@
+import Navbar from "./navbar.module.css";
+
 const NavBar = (props) => {
 	const { Username } = props
-	return <div>Navbar
-		<p>{Username}</p>
-		<ul>
-			<li>Home</li>
-			<li>My list</li>
-		</ul>
+	return (
+		<div className={styles.container}>
+			<div className={styles.wrapper}>Logo</div>
+			<a className={styles.logoLink} href="/"></a>
+			<div className={styles.logoWrapper}>Netflix</div>
+			<ul className={styles.navItems}>
+				<li className={styles.navItem}>Home</li>
+				<li className={styles.navItem2}>My list</li>
+			</ul>
 
-		<nav>
-			<div>
-				<button>
-					<p>{Username}</p>
-				</button>
+			<nav className={styles.navContainer}>
 				<div>
-					<a>Sign out</a>
+					<button className={styles.usernameBtn}>
+						<p className={styles.username}>{Username}</p>
+					</button>
+					<div>
+						<a>Sign out</a>
+					</div>
 				</div>
-			</div>
-		</nav>
-	</div>
+			</nav>
+		</div>
+	)
 }
 
 export default NavBar;
