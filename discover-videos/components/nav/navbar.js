ec84ae4b5aa9460129e4c04eaf13dec2d15eb40c
MacauleyStephenson <MacauleyStephenson@mail.com>
2022-02-19 18:45:37 +0000

    adjusting correct wrappers

--- discover-videos/components/nav/navbar.js
+++ discover-videos/components/nav/navbar.js
@@ -1,28 +1,35 @@
-import Navbar from "./navbar.module.css";
+import styles from "./navbar.module.css";
 
 const NavBar = (props) => {
 	const { Username } = props
 	return (
 		<div className={styles.container}>
-			<div className={styles.wrapper}>Logo</div>
-			<a className={styles.logoLink} href="/"></a>
-			<div className={styles.logoWrapper}>Netflix</div>
+			<div className={styles.wrapper}>
+				<a className={styles.logoLink} href="/">
+					<div className={styles.logoWrapper}>Netflix</div>
+				</a>
+			</div>
 			<ul className={styles.navItems}>
 				<li className={styles.navItem}>Home</li>
 				<li className={styles.navItem2}>My list</li>
 			</ul>
-
 			<nav className={styles.navContainer}>
 				<div>
 					<button className={styles.usernameBtn}>
 						<p className={styles.username}>{Username}</p>
+						{/* {expand more icon} */}
 					</button>
-					<div>
-						<a>Sign out</a>
+
+					<div className={styles.navDropdown}>
+						<div>
+							<a className={styles.linkName}>Sign out</a>
+							<div className={styles.lineWrapper}>
+							</div>
+						</div>
 					</div>
 				</div>
 			</nav>
-		</div>
+		</div >
 	)
 }
 
