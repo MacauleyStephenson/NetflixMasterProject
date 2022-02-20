546d4a273fe3da63e51c87d9030cc44e9e735386
MacauleyStephenson <MacauleyStephenson@mail.com>
2022-02-20 11:27:39 +0000

    implimented routing

--- discover-videos/components/nav/navbar.js
+++ discover-videos/components/nav/navbar.js
@@ -1,35 +1,52 @@
 import styles from "./navbar.module.css";
 
+import { useRouter } from "next/router";
+
 const NavBar = (props) => {
-	const { Username } = props
+	const { Username } = props;
+
+	const router = useRouter();
+
+	const handleOnClickHome = (e) => {
+		e.preventDefault()
+		router.push('/')
+	};
+
+	const handleOnClickMyList = (e) => {
+		e.preventDefault()
+		router.push('/browse/my-list')
+	};
+
 	return (
 		<div className={styles.container}>
 			<div className={styles.wrapper}>
 				<a className={styles.logoLink} href="/">
 					<div className={styles.logoWrapper}>Netflix</div>
 				</a>
-			</div>
-			<ul className={styles.navItems}>
-				<li className={styles.navItem}>Home</li>
-				<li className={styles.navItem2}>My list</li>
-			</ul>
-			<nav className={styles.navContainer}>
-				<div>
-					<button className={styles.usernameBtn}>
-						<p className={styles.username}>{Username}</p>
-						{/* {expand more icon} */}
-					</button>
 
-					<div className={styles.navDropdown}>
-						<div>
-							<a className={styles.linkName}>Sign out</a>
-							<div className={styles.lineWrapper}>
+				<ul className={styles.navItems}>
+					<li className={styles.navItem} onClick={handleOnClickHome}>Home</li>
+					<li className={styles.navItem2} onClick={handleOnClickMyList}>My list</li>
+				</ul>
+
+				<nav className={styles.navContainer}>
+					<div>
+						<button className={styles.usernameBtn}>
+							<p className={styles.username}>{Username}</p>
+							{/* {expand more icon} */}
+						</button>
+
+						<div className={styles.navDropdown}>
+							<div>
+								<a className={styles.linkName}>Sign out</a>
+								<div className={styles.lineWrapper}>
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-			</nav>
-		</div >
+				</nav>
+			</div>
+		</div>
 	)
 }
 
