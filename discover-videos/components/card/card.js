c8057907d82567bb8393b159a0bac442b438da5f
MacauleyStephenson <MacauleyStephenson@mail.com>
2022-03-01 08:29:36 +0000

    rendered card images

--- discover-videos/components/card/card.js
+++ discover-videos/components/card/card.js
@@ -1,7 +1,12 @@
+import Image from "next/image";
+
 const Card = (props) => {
 
 	const { imgUrl, size } = props;
-	return <div>Card</div>;
+	return <div>
+		Card
+		<Image src={imgUrl} alt="image" width='300px' height='300' />
+	</div>;
 };
 
 export default Card
