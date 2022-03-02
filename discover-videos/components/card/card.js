3493f802c150bf343d7f63b4306a04ba9c4a8308
MacauleyStephenson <MacauleyStephenson@mail.com>
2022-03-02 08:44:02 +0000

    error handling card component

--- discover-videos/components/card/card.js
+++ discover-videos/components/card/card.js
@@ -1,23 +1,33 @@
+import { setRequestMeta } from "next/dist/server/request-meta";
 import Image from "next/image";
+import { useState } from "react";
 import styles from './card.module.css';
 
 const Card = (props) => {
 
-	const { imgUrl, size } = props;
+	const { imgUrl = "/static/clifford.webp", size = "medium" } = props;
+
+	const [imgSrc, setImgSrc] = useState(imgUrl);
 
 	const classMap = {
 		'large': styles.lgItem,
 		'medium': styles.mdItem,
 		'small': styles.smItem,
 	}
+
+	const handleOnError = () => {
+		console.log('hii error');
+		setImgSrc("/static/clifford.webp")
+	}
 	return (
 		<div className={styles.container}>
 			Card
 			<div className={classMap[size]}>
 				<Image
-					src={imgUrl}
+					src={imgSrc}
 					alt="image"
 					layout="fill"
+					onError={handleOnError}
 					className={styles.cardImg} />
 			</div>
 		</div>
