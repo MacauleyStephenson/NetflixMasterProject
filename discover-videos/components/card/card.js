0e83de585938e89f94d86b803cfc76b431899888
MacauleyStephenson <MacauleyStephenson@mail.com>
2022-03-02 10:50:25 +0000

    fixed error handling solution

--- discover-videos/components/card/card.js
+++ discover-videos/components/card/card.js
@@ -1,11 +1,10 @@
-import { setRequestMeta } from "next/dist/server/request-meta";
 import Image from "next/image";
 import { useState } from "react";
 import styles from './card.module.css';
 
 const Card = (props) => {
 
-	const { imgUrl = "/static/clifford.webp", size = "medium" } = props;
+	const { imgUrl = "/static/cliffor.webp", size = "medium" } = props;
 
 	const [imgSrc, setImgSrc] = useState(imgUrl);
 
