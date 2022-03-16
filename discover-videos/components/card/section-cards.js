2768565c10a25a0dc923732f9fc9577595208d83
MacauleyStephenson <MacauleyStephenson@mail.com>
2022-03-16 09:57:49 +0000

    applied error handling for YouTube apis

--- discover-videos/components/card/section-cards.js
+++ discover-videos/components/card/section-cards.js
@@ -9,8 +9,10 @@ const SectionCards = (props) => {
 		<section className={styles.container}>
 			<h2 className={styles.title}>{title}</h2>
 			<div className={styles.cardWrapper}>
-				{videos.map((video, idx) => {
-					return <Card id={idx} imgUrl={video.imgUrl} size={size} />;
+				{videos.map((video, idx,) => {
+					return (
+						<Card id={idx} imgUrl={video.imgUrl} size={size} />
+					)
 				})}
 			</div>
 		</section>
