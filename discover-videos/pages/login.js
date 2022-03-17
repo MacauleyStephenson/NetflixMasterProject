59ac56fc89a316c115e6da83ba112919fcc1f064
MacauleyStephenson <MacauleyStephenson@mail.com>
2022-03-17 08:53:02 +0000

    added styling for login component

--- discover-videos/pages/login.js
+++ discover-videos/pages/login.js
@@ -1,4 +1,7 @@
 import Head from "next/head";
+import Image from "next/image";
+
+import styles from "../styles/login.module.css";
 
 const Login = () => {
 	return (
@@ -6,6 +9,18 @@ const Login = () => {
 			<Head>
 				<title>Netflix SignIn</title>
 			</Head>
+
+			<header>
+				<a className={styles.logoLink} href="/">
+					<div className={styles.logoWrapper}>
+						<Image
+							src={"/static/netflix.svg"}
+							alt="Netflix"
+							width="128px"
+							height="34px" />
+					</div>
+				</a>
+			</header>
 		</div>
 	)
 };
