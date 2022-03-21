ec475aa8e070e3d96dddeb9e5dbbdcab16e275f2
MacauleyStephenson <MacauleyStephenson@mail.com>
2022-03-21 08:22:40 +0000

    Completed Magic SDK implimentation

--- discover-videos/lib/magic-client.js
+++ discover-videos/lib/magic-client.js
@@ -1,7 +1,8 @@
 import { Magic } from "magic-sdk";
 
 const createMagic = () => {
-	return typeof window !== "undefined" && new Magic(process.env.MAGIC_PUBLISHABLE_API_KEY);
+	return typeof window !== "undefined"
+		&& new Magic(process.env.NEXT_PUBLIC_MAGIC_PUBLISHABLE_API_KEY);
 };
 
 const magic = createMagic();
