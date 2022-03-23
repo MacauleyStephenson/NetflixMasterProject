bdfe07f117849ee71436998a0a508593c0bd77ac
MacauleyStephenson <MacauleyStephenson@mail.com>
2022-03-23 09:37:09 +0000

    added banner

--- discover-videos/pages/index.js
+++ discover-videos/pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+import Banner from '../components/banner/banner'
+
 export default function Home() {
 	return (
 		<div className={styles.container}>
@@ -9,6 +11,11 @@ export default function Home() {
 				<title>Netflix</title>
 			</Head>
 			<h1>Netflix</h1>
+
+			<Banner />
+
+			{/* <Navbar />
+			<Card /> */}
 		</div>
 	)
 }
