42b743a78a2b5c33298fd12860797b5e0a960961
MacauleyStephenson <MacauleyStephenson@mail.com>
2022-03-24 10:21:05 +0000

    pressing on with routing

--- discover-videos/components/nav/navbar.js
+++ discover-videos/components/nav/navbar.js
@@ -1,8 +1,23 @@
 import styles from "./navbar.module.css";
 
+import { useRouter } from "next/router";
+import Link from "next/link";
 
 const NavBar = (props) => {
 	const { username } = props;
+
+	const router = useRouter();
+
+	const handleOnClickHome = (e) => {
+		e.preventDefault()
+		router.push("/");
+	};
+
+	const handleOnClickMyList = (e) => {
+		e.preventDefault()
+		router.push("/browse/my-list");
+	};
+
 	return (
 		<div className={styles.container}>
 			<div className={styles.wrapper}>
@@ -12,10 +27,10 @@ const NavBar = (props) => {
 					</div>
 				</a>
 				<ul className={styles.navItems}>
-					<li className={styles.navItem}>
+					<li className={styles.navItem} onClick={handleOnClickHome}>
 						Home
 					</li>
-					<li className={styles.navItem2}>
+					<li className={styles.navItem2} onClick={handleOnClickMyList}>
 						My List
 					</li>
 				</ul>
@@ -28,7 +43,11 @@ const NavBar = (props) => {
 
 						<div className={styles.navDropdown}>
 							<div>
-								<a className={styles.linkName}>Sign Out</a>
+								<Link href="/login">
+									<a className={styles.linkName}>
+										Sign Out
+									</a>
+								</Link>
 								<div className={styles.lineWrapper}></div>
 							</div>
 						</div>
