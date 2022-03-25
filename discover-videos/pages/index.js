70320a4978975798b670f58b3644a4c8b86518b2
MacauleyStephenson <MacauleyStephenson@mail.com>
2022-03-25 07:57:32 +0000

    implimented card and framer motion animation package

--- discover-videos/pages/index.js
+++ discover-videos/pages/index.js
@@ -5,6 +5,8 @@ import styles from '../styles/Home.module.css';
 import Banner from '../components/banner/banner';
 import NavBar from '../components/nav/navbar';
 
+import Card from '../components/card/card';
+
 export default function Home() {
 	return (
 		<div className={styles.container}>
@@ -15,9 +17,7 @@ export default function Home() {
 			<NavBar username="MacauleyS@gmail.com" />
 			<Banner title='Clifford the Red Dog' subTitle="A very cute dog!" imgUrl="/static/clifford.webp" />
 
-
-			{/*
-			<Card /> */}
+			<Card imgUrl="/static/clifford.webp" size="large" />
 		</div>
 	);
 }
