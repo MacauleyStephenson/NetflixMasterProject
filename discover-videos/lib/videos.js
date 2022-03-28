8f607f685e25963dcb17fb36a72e96ee5447d47b
MacauleyStephenson <MacauleyStephenson@mail.com>
2022-03-28 09:24:48 +0100

    proceeding with youtube api

--- discover-videos/lib/videos.js
+++ discover-videos/lib/videos.js
@@ -1,7 +1,17 @@
 import videoData from '../data/videos.json';
 
-export const getVideos = () => {
-	return videoData.items.map(item => {
+export const getVideos = async () => {
+
+	const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
+	const BASE_URL = "youtube.googleapis.com/youtube/v3";
+
+	const response = await fetch(
+		`https://${BASE_URL}/${url}&maxResults=25&key=${YOUTUBE_API_KEY}`
+	);
+
+	const data = await response.json();
+
+	return data?.items.map(item => {
 		return {
 			title: item.snippet.title,
 			imgUrl: item.snippet.thumbnails.high.url,
