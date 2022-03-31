37ec263d4ce018922b361a11b4162b565de454e7
MacauleyStephenson <MacauleyStephenson@mail.com>
2022-03-31 09:34:06 +0100

    completed sign in page structure

--- discover-videos/pages/login.js
+++ discover-videos/pages/login.js
@@ -3,6 +3,11 @@ import styles from "../styles/Login.module.css";
 import Image from "next/image";
 
 const Login = () => {
+
+	const handleLoginWithEmail = (e) => {
+		console.log("hi button");
+		e.preventDefault();
+	}
 	return (
 		<div>
 			<Head>
@@ -21,6 +26,24 @@ const Login = () => {
 						/>
 					</div>
 				</a>
+
+				<main className={styles.main}>
+					<div className={styles.mainWrapper}>
+						<h1 className={styles.signinHeader}>Sign In</h1>
+						<input
+							type="text"
+							placeholder="Email address"
+							className={styles.emailInput}
+						/>
+
+						<p className={styles.userMsg}></p>
+						<button
+							onClick={handleLoginWithEmail}
+							className={styles.loginBtn}>
+							Sign In
+						</button>
+					</div>
+				</main>
 			</header>
 		</div>
 	);
