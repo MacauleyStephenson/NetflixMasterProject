5fba73011843f5ab0294f00fe9955f5dc5bd666d
MacauleyStephenson <MacauleyStephenson@mail.com>
2022-03-31 09:01:00 +0100

    adding netflix logo to login page

--- discover-videos/pages/login.js
+++ discover-videos/pages/login.js
@@ -1,5 +1,29 @@
+import Head from "next/head";
+import styles from "../styles/Login.module.css";
+import Image from "next/image";
+
 const Login = () => {
-	return <div>Login</div>
+	return (
+		<div>
+			<Head>
+				<title>Login</title>
+				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+			</Head>
+
+			<header>
+				<a className={styles.logoLink} href="/">
+					<div className={styles.logoWrapper}>
+						<Image
+							src="/static/netflix.svg"
+							alt="Netflix logo"
+							width="128px"
+							height="34px"
+						/>
+					</div>
+				</a>
+			</header>
+		</div>
+	);
 };
 
 export default Login;
