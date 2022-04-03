0a5368cbc329eeec7cce89c382dce7f080b883c9
MacauleyStephenson <MacauleyStephenson@mail.com>
2022-04-03 11:59:58 +0100

    routes the user to login if they have not signed in

--- discover-videos/pages/_app.js
+++ discover-videos/pages/_app.js
@@ -1,7 +1,23 @@
+import { useRouter } from "next/router";
+import { useEffect } from 'react'
+
+import { magic } from "../lib/magic-client";
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	const router = useRouter();
+
+	useEffect(async () => {
+		const isLoggedIn = await magic.user.isLoggedIn();
+		if (isLoggedIn) {
+			//route to
+			router.push('/');
+		} else {
+			router.push('/login');
+		}
+	}, []);
+	//route to /login
+	return <Component {...pageProps} />
 }
 
 export default MyApp
