1af47b320692b8741147487f8b5a20e76a76bc72
MacauleyStephenson <MacauleyStephenson@mail.com>
2022-04-06 07:18:51 +0100

    routed flicker with login

--- discover-videos/pages/_app.js
+++ discover-videos/pages/_app.js
@@ -1,23 +1,38 @@
 import { useRouter } from "next/router";
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 import { magic } from "../lib/magic-client";
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
 	const router = useRouter();
+	const [isLoading, setIsLoading] = useState(true);
 
 	useEffect(async () => {
 		const isLoggedIn = await magic.user.isLoggedIn();
 		if (isLoggedIn) {
-			//route to
+			//route to /
 			router.push('/');
 		} else {
 			router.push('/login');
 		}
 	}, []);
+
+	useEffect(() => {
+		const handleComplete = () => {
+			setIsLoading(false);
+		};
+		router.events.on('routeChangeComplete', handleComplete);
+		router.events.on('routeChangeError', handleComplete);
+
+		return () => {
+			router.events.off('routeChangeComplete', handleComplete);
+			router.events.off('routeChangeError', handleComplete);
+		}
+	}, [router]);
+
 	//route to /login
-	return <Component {...pageProps} />
+	return isLoading ? <div>Loading...</div> : <Component {...pageProps} />
 }
 
 export default MyApp
