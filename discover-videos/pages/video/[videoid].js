e65a7f0ab8d74b317c4b16f35ae0d4cc266353a3
MacauleyStephenson <MacauleyStephenson@mail.com>
2022-04-09 09:46:08 +0100

    formulating modal component

--- discover-videos/pages/video/[videoid].js
+++ discover-videos/pages/video/[videoid].js
@@ -1,13 +1,27 @@
 import { useRouter } from "next/router";
 import Modal from "react-modal";
+import styles from '../../styles/Video.module.css';
 
 Modal.setAppElement("#__next");
 
 const Video = () => {
 	const router = useRouter();
 	console.log({ router });
-	return <div>Video page {router.query.videoId}</div>
+	return (
+		<div>
+			Video page {router.query.videoId}
+			<Modal
+				isOpen={true}
+				contentLabel="Watch the video"
+				onRequestClose={() => router.back()}
+				overlayClassName={styles.overlay}
 
+
+			>
+				<div>Modal body</div>
+			</Modal>
+		</div>
+	);
 };
 
 export default Video;
