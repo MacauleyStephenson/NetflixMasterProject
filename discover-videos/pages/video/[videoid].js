26c549047f462b9f883b7b8564f345f342b8b1de
MacauleyStephenson <MacauleyStephenson@mail.com>
2022-04-09 09:52:19 +0100

    created styling for modal

--- discover-videos/pages/video/[videoid].js
+++ discover-videos/pages/video/[videoid].js
@@ -8,15 +8,13 @@ const Video = () => {
 	const router = useRouter();
 	console.log({ router });
 	return (
-		<div>
-			Video page {router.query.videoId}
+		<div className={styles.container}>
 			<Modal
 				isOpen={true}
 				contentLabel="Watch the video"
 				onRequestClose={() => router.back()}
+				className={styles.modal}
 				overlayClassName={styles.overlay}
-
-
 			>
 				<div>Modal body</div>
 			</Modal>
