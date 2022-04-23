36e57c1af909645a456b88fa1e2cfe9a4b0430ec
MacauleyStephenson <MacauleyStephenson@mail.com>
2022-04-23 11:54:54 +0100

    Continuing to create API with JWT

--- discover-videos/pages/api/login.js
+++ discover-videos/pages/api/login.js
@@ -15,14 +15,16 @@ export default async function login(req, res) {
 			//create jwt
 			const token = jwt.sign({
 				...metadata,
-				"iat": Math.floor(Date.now() / 1000),
-				"exp": Math.floor(Date.now() / 100 + 7 * 24 * 60 * 60),
+				iat: Math.floor(Date.now() / 1000),
+				exp: Math.floor(Date.now() / 1000 + 7 * 24 * 60 * 60),
 				"https://hasura.io/jwt/claims": {
 					"x-hasura-allowed-roles": ["user", "admin"],
 					"x-hasura-default-role": "user",
 					"x-hasura-user-id": `${metadata.issuer}`,
 				},
-			});
+			},
+				"thisisasecretthisisasecret1234567"
+			);
 			console.log({ token });
 
 			res.send({ done: true });
